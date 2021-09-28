181c1208fed036222ddf91c355206a0428356074
fabyuribe <fabiola.uribe@tec.mx>
2021-09-27 19:59:39 -0500

    SoloListas

--- assignments/15SumaColumnas/src/exercise.py
+++
@@ -1,9 +0,0 @@
-# Escribe aquí tus funciones
-
-def main():
-    # Escribe aquí las instrucciones principales del programa
-
-
-
-if __name__=='__main__':
-    main()
--- assignments/18CentroMatriz/src/exercise.py
+++
@@ -1,8 +0,0 @@
-# EScribe aquí tus funciones
-
-def main():
-    # Escribe aquí las instrucciones prinicipales del programa
-
-
-if __name__=='__main__':
-    main()
